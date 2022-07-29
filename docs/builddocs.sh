c62ba0a3254bad204faeaff4f4d54027d74eac39
jede <jede@oric.org>
2022-07-29 15:32:14 +0200

    First init

---
+++ docs/builddocs.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+HOMEDIR=build/
+HOMEDIRDOC=docs/
+mkdir build/usr/share/man -p
+mkdir -p ../build/usr/share/man/
+LIST_COMMAND='ftdos'
+echo Generate hlp
+for I in $LIST_COMMAND; do
+echo Generate $I
+cat $HOMEDIRDOC/$I.md | md2hlp.py3 -c docs/md2hlp.cfg > build/usr/share/man/$I.hlp
+done
