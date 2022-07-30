211509bca51bca994dc5b4e7f6b35ee837584612
jede <jede@oric.org>
2022-07-30 15:13:42 +0200

    update

--- docs/builddocs.sh
+++ docs/builddocs.sh
@@ -7,5 +7,5 @@ LIST_COMMAND='ftdos'
 echo Generate hlp
 for I in $LIST_COMMAND; do
 echo Generate $I
-cat $HOMEDIRDOC/$I.md | md2hlp.py3 -c docs/md2hlp.cfg > build/usr/share/man/$I.hlp
+cat $HOMEDIRDOC/$I.md | ../md2hlp/src/md2hlp.py3 -c docs/md2hlp.cfg > build/usr/share/man/$I.hlp
 done
